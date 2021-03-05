2289493dd2c2102fd74d23f19b627b3a50043bc1
Bilal Sheikh <bilalsheikh20@hotmail.com>
2021-03-04 23:11:18 -0500

    updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,10 @@
 /**
  * Add your code here to fix URI connection
- * i added this code brooooo
- * module.exports ={
-
-    "URI": "mongodb://localhost/contact"
-}
  * 
  */
-module.exports ={
+module.exports =
+{
 
-    "URI": "mongodb://localhost/contact"
-    //"URI": "mongodb+srv://<username>:<Password.123>@<your-cluster-url>/favBooks?retryWrites=true&w=majority"
+    //"URI": "mongodb://localhost/"
+    "URI": "mongodb+srv://username:Password.123@cluster0.jgv7n.mongodb.net/favBooks?retryWrites=true&w=majority"
 }
